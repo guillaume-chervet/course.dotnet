a882f9d34ec4acd455e0d896b4d0baad294bdfdc
Guillaume Chervet <guillaume.chervet@gmail.com>
2020-10-25 20:29:31 +0100

    rename projects

--- Basket/tests/BasketCore.Tests/BasketService_CalculateBasketAmoutShould.cs
+++ Basket/tests/BasketCore.Tests/BasketService_CalculateBasketAmoutShould.cs
@@ -4,10 +4,11 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using Basket;
 using Newtonsoft.Json;
 using Xunit;
 
-namespace Basket.Tests
+namespace BasketCore.Tests
 {
     public class BasketService_CalculateBasketAmoutShould
     {
@@ -91,26 +92,26 @@ namespace Basket.Tests
             Assert.Equal(basketTest.ExpectedPrice, amountTotal);
         }
 
-        /*public static ArticleDatabase GetArticleDatabaseMock(string id)
+        public static Task<ArticleDatabase> GetArticleDatabaseMockAsync(string id)
         {
             switch (id)
             {
                 case "1":
-                    return new ArticleDatabase {Id = "1", Price = 1, Stock = 35, Label = "Banana", Category = "food"};
+                    return Task.FromResult(new ArticleDatabase {Id = "1", Price = 1, Stock = 35, Label = "Banana", Category = "food"});
                 case "2":
-                    return new ArticleDatabase
+                    return Task.FromResult(new ArticleDatabase
                     {
                         Id = "2",
                         Price = 500,
                         Stock = 20,
                         Label = "Fridge electrolux",
                         Category = "electronic"
-                    };
+                    });
                 case "3":
-                    return new ArticleDatabase {Id = "3", Price = 49, Stock = 68, Label = "Chair", Category = "desktop"};
+                    return Task.FromResult(new ArticleDatabase {Id = "3", Price = 49, Stock = 68, Label = "Chair", Category = "desktop"});
                 default:
                     throw new NotImplementedException();
             }
-        }*/
+        }
     }
 }
